0d6d147fb3af3a5d2a2f425080ca1a77cf4e7eb0
Lucifer <690699341@qq.com>
2021-04-02 14:05:55 +0800

    删除多余文件

--- zipLocalHelper.js
+++
@@ -1,8 +0,0 @@
-var zipper = require("zip-local");
-
-exports.directoryToFile=(localPath)=>{
-    zipper.sync.zip(localPath).compress().save(localPath.replace('Release','1.tar'));
-    // cwd：需要压缩源文件的路径
-    // src：需要压缩的文件 ** 是全部文件
-    // dest 是解压后的层级，如果不设置会使用src的全部层级
-}
